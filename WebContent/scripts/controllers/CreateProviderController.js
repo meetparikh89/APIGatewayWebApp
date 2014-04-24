59b42fb067daec948f9cacfc08ca73353fe0c8c4
meetparikh89 <meet2689@gmail.com>
2014-04-24 14:10:12 +0530

    Create Provider completed.

--- WebContent/scripts/controllers/CreateProviderController.js
+++ WebContent/scripts/controllers/CreateProviderController.js
@@ -12,9 +12,30 @@ AdminUtilityApp.controller('CreateProviderController', ['$scope','$http','$locat
 		return true;
 	}
 
+	function generateDataObject(){
+		var bodyData = {};
+		bodyData['base_uri'] = $scope.providerBaseUrl;
+		if(!($scope.providerCertificate == null || $scope.providerCertificate === undefined || $scope.providerCertificate.trim() == '' )){
+			bodyData['certificate'] = $scope.providerCertificate;
+		}
+		return bodyData;
+	}
+
 	$scope.createProvider = function() {
 		if(validateInput()){
-
+			var bodyData = generateDataObject();
+			console.log(bodyData);
+			$http({
+				method: 'PUT',
+				url : 'admin/provider/' + $scope.providerName,
+				data : bodyData
+			})
+			.success(function(data){
+				$scope.createProvider_message = 'Successfully created provider';
+			})
+			.error(function(data){
+				$scope.createProvider_message = data.error_description;
+			});
 		}
 	}
 }]);

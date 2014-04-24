b7fde611c4edfeb95a2ca3fca7a3188b2b50cc9e
meetparikh89 <meet2689@gmail.com>
2014-04-24 17:09:06 +0530

    Beautifying manage provider info page.

--- WebContent/scripts/controllers/ProviderDetailsController.js
+++ WebContent/scripts/controllers/ProviderDetailsController.js
@@ -13,33 +13,34 @@ AdminUtilityApp.controller("ProviderDetailsController",
 			$scope.providerInfo = data;
 			//console.log(JSON.stringify(data));
 			
-			var capList =  new Array();
-			var lenCap = data.capabilities.length;
-			for (var itr=0; itr < lenCap; itr++) {
-				var objCap = data.capabilities[itr];
-				var capability = new Object();
-				capability["name"] = objCap["name"];
-				
-				var lenRoute = objCap.routes.length;
-				var routeList = new Array();
-				for (var itrRoute=0; itrRoute < lenRoute; itrRoute++) {
-					var objRoute = objCap.routes[itrRoute];
-					var route = new Object();
-					route["path"] = objRoute["path"];
-					var lenMethod = objRoute.methods.length;
-					var methods = "";
-					for(var itrM = 0; itrM < lenMethod; itrM++) {
-						if(methods.length > 0)
-							methods = methods + ",";
-						methods = methods + objRoute.methods[itrM];
-					}
-					route["methods"] = methods;
-					routeList.push(route);
-				}
-				capability["routes"] = routeList;
-				capList.push(capability);
-			}
-			$scope.capabilities = capList;
+//			<------------------ Use below code in case of multiple methods for single path -------------->
+//			var capList =  new Array();
+//			var lenCap = data.capabilities.length;
+//			for (var itr=0; itr < lenCap; itr++) {
+//				var objCap = data.capabilities[itr];
+//				var capability = new Object();
+//				capability["name"] = objCap["name"];
+//				
+//				var lenRoute = objCap.routes.length;
+//				var routeList = new Array();
+//				for (var itrRoute=0; itrRoute < lenRoute; itrRoute++) {
+//					var objRoute = objCap.routes[itrRoute];
+//					var route = new Object();
+//					route["path"] = objRoute["path"];
+//					var lenMethod = objRoute.methods.length;
+//					var methods = "";
+//					for(var itrM = 0; itrM < lenMethod; itrM++) {
+//						if(methods.length > 0)
+//							methods = methods + ",";
+//						methods = methods + objRoute.methods[itrM];
+//					}
+//					route["methods"] = methods;
+//					routeList.push(route);
+//				}
+//				capability["routes"] = routeList;
+//				capList.push(capability);
+//			}
+			$scope.capabilities = data.capabilities;
 			
 		})
 		.error(function(data){
